0393bb54c5ebfd6a1728a62c1800b3d7bf71e0c1
Austin Moore <austin_moore@me.com>
2015-03-27 19:02:09 -0600

    Added basic streaming capabilities
    It is now possible for clients to watch a host on the website, but they must declare if they
    are watching or hosting before it will work.

--- src/main/webapp/js/editor.js
+++ src/main/webapp/js/editor.js
@@ -1,13 +1,70 @@
 sam = "editor.js loaded";
 
+/**
+ * Use after the page has loaded to let the client know that it's a host.
+ * We need this function because we don't currently have a way for the client to decide if it's a host or a viewer.
+ * TODO: Remove this and automatically tell the browser it's a host.
+ */
+function setHost() {
+    registerChangeListener();
+}
 
+/**
+ * Use after the page has loaded to let the client know that it's a viewer.
+ * We need this function because we don't currently have a way for the client to decide if it's a host or a viewer.
+ * TODO: Remove this and automatically tell the browser it's a viewer.
+ */
+function setViewer() {
+    startEventSource();
+}
 
-$(document).ready(function () {
-    var codeMirror = CodeMirror($('#editor').get()[0], {
-        value: "function myScript()\n{\n   return 100;\n}\n",
+/**
+ * Tells the browser to send a POST every time the user make a change in the text editor.
+ */
+function registerChangeListener() {
+    codeMirror.on("changes", function(editor, change) {
+        $.ajax({
+            type: "POST",
+            url: "Update",
+            data: {change: JSON.stringify(change)}
+        });
+    });
+}
+
+/**
+ * Sets up an Event Source, which will allow the server to 'push' information to the client.
+ */
+function startEventSource() {
+    // Check if the browser supports Server-Sent Events
+    if (typeof(EventSource) !== "undefined") {
+        // Create a new Event Source to receive updates from /Update
+        var source = new EventSource("Update");
+
+        // When the server sends info...
+        source.onmessage = function(event) {
+            // Get the changes from the request.
+            var change = JSON.parse(event.data);
+
+            // Loop through all the change objects (usually one, but sometimes there's two).
+            for (var i = 0; i < change.length; i++) {
+                codeMirror.replaceRange(change[i].text, change[i].from,
+                                        change[i].to, change[i].origin);
+            }
+        };
+    } else {
+        $("#editor").empty();
+        $("#editor").append("<h1>Your browser is not supported!</h1>");
+    }
+}
+
+/**
+ * Set up CodeMirror when the page is ready.
+ */
+$(document).ready(function() {
+    var editorDiv = $('#editor').get()[0];
+    codeMirror = CodeMirror(editorDiv, {
+        value: "function myScript()\n{\n  return 100;\n}\n",
         mode: "javascript",
         lineNumbers: true
     });
-    debugger;
-    console.log(codeMirror);
 });
